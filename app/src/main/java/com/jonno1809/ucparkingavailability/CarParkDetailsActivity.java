2dc8e3bcabbd025f8149f37bc2193c9b07038b5b
Jonathan Simmons <jonathan.p.simmons96@gmail.com>
2018-03-23 16:58:31 +1100

    Add up activity

--- app/src/main/java/com/jonno1809/ucparkingavailability/CarParkDetailsActivity.java
+++ app/src/main/java/com/jonno1809/ucparkingavailability/CarParkDetailsActivity.java
@@ -3,8 +3,10 @@ package com.jonno1809.ucparkingavailability;
 import android.content.Intent;
 import android.os.Bundle;
 
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class CarParkDetailsActivity extends AppCompatActivity {
@@ -15,6 +17,8 @@ public class CarParkDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_car_park_details);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         Intent intent = getIntent();
         CarPark carPark = intent.getParcelableExtra("carPark");
@@ -41,4 +45,14 @@ public class CarParkDetailsActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
