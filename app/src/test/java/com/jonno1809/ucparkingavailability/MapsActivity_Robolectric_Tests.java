96e24c8b55ee3041b00a65f615516b0b976ab8e6
Jonathan Simmons <jonathan.p.simmons96@gmail.com>
2018-06-10 15:29:40 +1000

    Add tests for changing map type

--- app/src/test/java/com/jonno1809/ucparkingavailability/MapsActivity_Robolectric_Tests.java
+++ app/src/test/java/com/jonno1809/ucparkingavailability/MapsActivity_Robolectric_Tests.java
@@ -1,5 +1,8 @@
 package com.jonno1809.ucparkingavailability;
 
+import android.view.MenuItem;
+
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.PolygonOptions;
 
@@ -10,6 +13,7 @@ import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
+import org.robolectric.fakes.RoboMenuItem;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -26,7 +30,7 @@ public class MapsActivity_Robolectric_Tests {
         List<LatLng> coords = new LinkedList<>();
         coords.add(new LatLng(-35.240344, 149.086734));
         coords.add(new LatLng(-35.240344, 149.086573));
-        carPark = new CarPark("K1", 27, 0, 0,  "Childcare pickup/dropoff", new LatLng
+        carPark = new CarPark("K1", 27, 0, 0, "Childcare pickup/dropoff", new LatLng
                 (-35.240708, 149.086687), new PolygonOptions().addAll(coords));
         mapsActivity = Robolectric.setupActivity(MapsActivity.class);
     }
@@ -39,5 +43,37 @@ public class MapsActivity_Robolectric_Tests {
     @Test
     public void fragementContainerShouldNotBeNull() {
         Assert.assertNotNull(mapsActivity.getSupportFragmentManager().findFragmentById(R.id.fragmentContainer));
+    }
+
+    @Test
+    public void testMenuItemsContainCorrectStrings() {
+        MenuItem satelliteMenuItem = new RoboMenuItem(R.id.satelliteMapMenuItem);
+        MenuItem plainMenuItem = new RoboMenuItem(R.id.normalMapMenuItem);
+        MenuItem terrainMenuItem = new RoboMenuItem(R.id.terrainMapMenuItem);
+        MenuItem hybridMenuItem = new RoboMenuItem(R.id.hybridMapMenuItem);
+        Assert.assertEquals("Satellite view", satelliteMenuItem.getTitle());
+        Assert.assertEquals("Normal view", plainMenuItem.getTitle());
+        Assert.assertEquals("Terrain view", terrainMenuItem.getTitle());
+        Assert.assertEquals("Hybrid view", hybridMenuItem.getTitle());
+    }
+
+    @Test
+    public void testMapTypeMenuItemsDisplayCorrectMapType() {
+        MenuItem menuItem = new RoboMenuItem(R.id.satelliteMapMenuItem);
+        GoogleMap googleMap = mapsActivity.getMap();
+        mapsActivity.onOptionsItemSelected(menuItem);
+        Assert.assertEquals(GoogleMap.MAP_TYPE_SATELLITE, googleMap.getMapType());
+
+        menuItem = new RoboMenuItem(R.id.normalMapMenuItem);
+        mapsActivity.onOptionsItemSelected(menuItem);
+        Assert.assertEquals(GoogleMap.MAP_TYPE_NORMAL, googleMap.getMapType());
+
+        menuItem = new RoboMenuItem(R.id.terrainMapMenuItem);
+        mapsActivity.onOptionsItemSelected(menuItem);
+        Assert.assertEquals(GoogleMap.MAP_TYPE_TERRAIN, googleMap.getMapType());
+
+        menuItem = new RoboMenuItem(R.id.hybridMapMenuItem);
+        mapsActivity.onOptionsItemSelected(menuItem);
+        Assert.assertEquals(GoogleMap.MAP_TYPE_HYBRID, googleMap.getMapType());
     }
 }
